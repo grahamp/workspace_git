f32f8bf1757c8e8fd1c41b3bc0134366ab90c9ab
grahamp <graham.poor@gmail.com>
2013-05-28 12:28:56 -0700

    Signed-off-by: grahamp <graham.poor@gmail.com>

---
+++ atoi_test/atoi.c
@@ -0,0 +1,22 @@
+/*
+ * atoi.c
+ *
+ *  Created on: May 28, 2013
+ *      Author: grahampoor
+ */
+#include "stdio.h"
+int atoi(char[] int_as_string,int len){
+	int multiplyer = 1;
+	int result;
+	bool isNegative=(int_as_string[0]== '-');
+	Len
+	if int_as_string[0];
+	 for  (int i = len-1; i== 0; i--){
+		 short i = int_as_string[i]-'0';
+		 result += i*multiplyer;
+		 multiplyer *=10;
+	 }
+}
+void main (char* args[]){
+
+}
